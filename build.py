9a26e7cfb9428034350dc407b844628c836d5266
memsharded <james@conan.io>
2018-03-01 19:30:24 +0100

    simplified recipe

--- build.py
+++ build.py
@@ -4,5 +4,16 @@ import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    if platform.system() == "Windows":
+        builder.add(settings={"compiler":"Visual Studio", "compiler.version": "15",
+                          "arch": "x86", "build_type": "Release"},
+                    options={}, env_vars={}, build_requires={})
+    elif platform.system() == "Linux":
+        builder.add(settings={"compiler":"gcc", "compiler.version": "7",
+                              "arch": "x86_64", "build_type": "Release"},
+                    options={}, env_vars={}, build_requires={})
+    else:
+        builder.add(settings={"compiler":"apple-clang", "compiler.version": "9",
+                              "arch": "x86_64", "build_type": "Release"},
+                    options={}, env_vars={}, build_requires={})
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,13 @@ import os
 
 
 class PyBind11Conan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
     name = "pybind11"
     version = "2.2.2"
+    description = "Seamless operability between C++11 and Python"
+    homepage = "https://github.com/pybind/pybind11"
     license = "BSD Style: https://github.com/pybind/pybind11/blob/master/LICENSE"
     url = "https://github.com/memsharded/conan-pybind11.git"
+    no_copy_sources = True
 
     def source(self):
         tools.download("https://github.com/pybind/pybind11/archive/v%s.tar.gz" % self.version,
@@ -15,8 +17,6 @@ class PyBind11Conan(ConanFile):
         tools.unzip("pybind11.tar.gz")
         os.unlink("pybind11.tar.gz")
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure(defs={"PYBIND11_TEST": False}, source_folder="pybind11-%s" % self.version)
-        cmake.build()
-        cmake.install()
+    def package(self):
+        self.copy("*", src="pybind11-%s/include" % self.version, dst="include")
+        self.copy("*LICENSE", keep_path=False)
