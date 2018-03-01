d9cfb069fceb804ffeb51caa61ad47bd6d079ace
memsharded <james@conan.io>
2018-03-01 19:33:48 +0100

    wrong VS arch

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     if platform.system() == "Windows":
         builder.add(settings={"compiler":"Visual Studio", "compiler.version": "15",
-                          "arch": "x86", "build_type": "Release"},
+                              "arch": "x86_64", "build_type": "Release"},
                     options={}, env_vars={}, build_requires={})
     elif platform.system() == "Linux":
         builder.add(settings={"compiler":"gcc", "compiler.version": "7",
