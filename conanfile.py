f58f75ed78642b751c93e8798a92660e417d9c81
memsharded <james@conan.io>
2018-03-05 09:35:54 +0100

    using find.cmake

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,15 @@
-from conans import ConanFile, tools
+from conans import ConanFile, tools, CMake
 import os
 
 
 class PyBind11Conan(ConanFile):
     name = "pybind11"
     version = "2.2.2"
+    settings = "os", "compiler", "arch", "build_type"
     description = "Seamless operability between C++11 and Python"
     homepage = "https://github.com/pybind/pybind11"
     license = "BSD Style: https://github.com/pybind/pybind11/blob/master/LICENSE"
-    url = "https://github.com/memsharded/conan-pybind11.git"
+    url = "https://github.com/conan-community/conan-pybind11"
     no_copy_sources = True
 
     def source(self):
@@ -17,6 +18,14 @@ class PyBind11Conan(ConanFile):
         tools.unzip("pybind11.tar.gz")
         os.unlink("pybind11.tar.gz")
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(defs={"PYBIND11_TEST": False}, source_folder="pybind11-%s" % self.version)
+        cmake.build()
+        cmake.install()
+
     def package(self):
-        self.copy("*", src="pybind11-%s/include" % self.version, dst="include")
         self.copy("*LICENSE", keep_path=False)
+  
+    def package_id(self):
+        self.info.header_only()
